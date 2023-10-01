ca923a5f4394da50dced0628b7c6b7399d537c25
Pedro Pablo <pmontesarroyave@gmail.com>
2023-10-01 15:50:41 -0500

    podemos manejar estados

--- src/CreateButton.js
+++ src/CreateButton.js
@@ -3,7 +3,14 @@ import './CreateButton.css'
 function CreateButton() {
     return (
         <div className="custom-button-container">
-            <button className='custom-button'>Crear</button>
+            <button className='custom-button'
+                onClick={(event)=>{
+                    console.log(`precionaste muy fuerte el boton`);
+                    console.log(event)
+                    console.log(event.target)
+                    
+                }}
+            >Crear</button>
         </div >
     )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import './TodoSearch.css'
 
 
 function TodoSearch() {
-    return (
+    const [searchValue, setSearchValue] = React.useState('')
+    console.log(`los usuariso buscano con el valor de ${searchValue}`)
 
+
+    return (
         <input
             type="text"
             className="custom-input"
-            placeholder="Dormir 32 minutos" />
+            placeholder="Dormir 32 minutos"
+            value={searchValue}
+            onChange={(event) => {
+                const cambio = event.target.value
+                setSearchValue(cambio)
+
+            }}
+        />
     )
 }
 
